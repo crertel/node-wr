d4af6db7941409d44ee30f96ca895c3d238a69ce
Chris Ertel <chris@kedagital.com>
2016-02-18 02:27:21 -0600

    Add websocket listener.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var server = require('http').createServer()
 var wr = require('./water_rower.js')({ debug: false} );
 var ws = require('ws');
+var url = require('url')
+var WebSocketServer = ws.Server
+var wsServer = new WebSocketServer({ server: server });
 
 var parseArgs = require('minimist');
 var argv = parseArgs( process.argv , {
@@ -14,14 +18,31 @@ var comport = argv.comport;
 
 var rower = new wr.WaterRower({ port: comport });
 
-
 var express = require('express');
 var app = express();
 app.get('/', function(req, res) {
   res.status(200).send('Hello, world!');
 });
 
+wsServer.on('connection', function connection(sock) {
+  var location = url.parse(ws.upgradeReq.url, true);
+  var updateListener = function(msg)
+    sock.send( JSON.stringify(msg) );
+  };
+
+  rower.on('readings', updateListener);
+
+  sock.on('disconnect', function(){
+    rower.removeListener(updateListener);
+    console.log("Socket disconnect.");
+  });
+
+  console.log("Socket connect.");
+});
+
+
 console.log('Starting water rower.');
-app.listen( port, function () {
-  console.log('Starting water rower server.');
+server.on('request', app);
+server.listen(port, function () {
+  console.log('Listening on ' + server.address().port)
 });
